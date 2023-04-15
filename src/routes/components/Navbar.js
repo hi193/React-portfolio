561f6e2128d9169b2e1546e9e93341470e3c9f8a
hims <76097755+hi193@users.noreply.github.com>
2023-04-15 20:31:20 +0530

    commit

---
+++ src/routes/components/Navbar.js
@@ -0,0 +1,16 @@
+import "./navbar.css"
+import React from 'react'
+import {link} from "react-router-dom"
+
+const Navbar  = () => {
+  return (
+    <div className ="header">
+        <link to="/" /> 
+        <h1>Portfolio</h1>
+       
+        
+        </div>
+  )
+}
+
+export default Navbar
